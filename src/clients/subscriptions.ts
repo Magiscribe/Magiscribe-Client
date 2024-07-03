3e74ec33064df22afa81fbad20f139cf36e2eb79
Kyle Lierer <liererkt@gmail.com>
2024-07-02 20:47:57 -0500

    Removed prompt

--- src/clients/subscriptions.ts
+++ src/clients/subscriptions.ts
@@ -4,8 +4,8 @@ export const GRAPHQL_SUBSCRIPTION = gql`
   subscription predictionAdded($subscriptionId: String!) {
     predictionAdded(subscriptionId: $subscriptionId) {
       id
+      subscriptionId
       context
-      prompt
       result
       type
     }
