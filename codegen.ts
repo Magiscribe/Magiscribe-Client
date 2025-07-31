09404c45824971e10dc769da394b8455304708dd
unknown <management@schwartztutoring.com>
2025-07-30 20:19:15 -0500

    Fix issue

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,9 @@
-import type { CodegenConfig } from '@/graphql-codegen/cli';
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
   ignoreNoDocuments: true,
-  schema: 'http://localhost:3000/graphql',
+  schema: 'http://127.0.0.1:3000/graphql',
   documents: ['src/**/*.tsx', 'src/**/*.ts'],
   hooks: {
     afterAllFileWrite: ['prettier --write'],
