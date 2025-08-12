1c03295abbf475d6cde2643b8f1ea9aae6651246
Kyle Lierer <liererkt@gmail.com>
2025-08-11 23:58:49 -0500

    Changed how metadata is handled

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import codegen from 'vite-plugin-graphql-codegen';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { injectMetaTags } from './src/plugins/vite-meta-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
-    injectMetaTags(),
     codegen({
       runOnBuild: false, // Do not run codegen on build, the CI pipeline does not have access to the server
       runOnStart: true, // Run codegen on start, this is useful for local development
